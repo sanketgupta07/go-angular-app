998aced5a8b01b0090bb8aeef0fbbe2f904df50a
Sanket Gupta <sanketgupta07@gmail.com>
2020-09-10 23:26:45 +0530

    Update

--- src/app/utils/util.service.ts
+++ src/app/utils/util.service.ts
@@ -1,4 +1,6 @@
 import { HttpHeaders } from '@angular/common/http';
+import { Injectable } from "@angular/core";
+@Injectable()
 export class Utils {
     constructor(){}
     public setHeaders(header: HttpHeaders){
